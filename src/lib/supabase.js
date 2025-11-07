9c2558defc0bb95ad51c4b779b5b9288bdf36997
cmonsalt <cristiantm12@gmail.com>
2025-11-07 13:11:06 -0500

    mensaje

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://lppgwmkkvxwyskkcvsib.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZocWR4c2l4Y2RiZGx0aWp4bnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5NjA1NTYsImV4cCI6MjA1MTUzNjU1Nn0.gqKcN_bHDQKXwwEGKsRDtQ2kpDKO7rUgTXQgvnxq1Yc'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwcGd3bWtrdnh3eXNra2N2c2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NjczMTgsImV4cCI6MjA3NzE0MzMxOH0.MxP-iE4M9q4JSXUosWxeo3ZsLm-blQOCMpYp8OpA_5c'
 )
