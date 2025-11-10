e5fd84dabbceb2d503c8aeffb4662cf6cfadf191
cmonsalt <cristiantm12@gmail.com>
2025-11-10 17:42:52 -0500

    actualización

--- src/pages/ChatView.jsx
+++ src/pages/ChatView.jsx
@@ -570,13 +570,7 @@ async function handleConsultarIA() {
                       )}
 
                       <p className="text-sm">{msg.message}</p>
-
-                      {msg.is_ppv && (
-  <>
-    {console.log('🎯 PPV:', msg.of_message_id)}
-    <PPVMessage message={msg} />
-  </>
-)}
+                      
 
                       {msg.media_url && (
                         msg.is_ppv ? (
